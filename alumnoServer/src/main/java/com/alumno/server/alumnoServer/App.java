268e8e46bec5aa32d544194a934a892be57e6c55
Javiertries2code <superjabo@live.com>
2025-01-17 19:15:13 +0100

    inicial commit

---
+++ alumnoServer/src/main/java/com/alumno/server/alumnoServer/App.java
@@ -0,0 +1,38 @@
+package com.alumno.server.alumnoServer;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import com.alumno.server.alumnoServer.socketIO.SocketIOModule;
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOServer;
+
+public class App {
+	
+	private static  String HOST_NAME = "10.0.22.247";
+	private static final int PORT = 4000;
+	
+	public static void main(String[] args) {
+		//find my connection
+	     try {
+	            // Obtener la dirección IP de la máquina local
+	            InetAddress localHost = InetAddress.getLocalHost();
+	           // HOST_NAME = localHost.getHostAddress();
+	            System.out.println("Nombre del host: " + localHost.getHostName());
+	            System.out.println("Dirección IP: " + localHost.getHostAddress());
+	        } catch (UnknownHostException e) {
+	            System.err.println("Error al obtener la dirección IP: " + e.getMessage());
+	        }
+		
+		
+		// Server configuration 
+		Configuration config = new Configuration ();
+		config.setHostname(HOST_NAME);
+		config.setPort(PORT);
+		
+		// We start the server
+		SocketIOServer server = new SocketIOServer(config);
+		SocketIOModule module = new SocketIOModule(server);
+		module.start();
+	}
+}
